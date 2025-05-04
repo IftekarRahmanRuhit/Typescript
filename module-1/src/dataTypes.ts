60164219dbadaf518342415f3e796cd7d5cb032f
Iftekar Rahman Ruhit <iftekarrahmanruhit2003@gmail.com>
2025-05-04 22:16:21 +0600

    data types

---
+++ module-1/src/dataTypes.ts
@@ -0,0 +1,23 @@
+// Data Types
+
+// string
+let firstName : string = " Iftekar";
+
+// number
+let age : number = 25;
+
+// boolean
+let isStudent : boolean = true;
+
+// null
+let isNull : null = null;
+
+// undefined
+let isUndefined : undefined = undefined;
+
+//Array
+let numbers : number[] = [1, 2, 3, 4, 5];
+let names : string[] = ["Iftekar", "Rahman", "Ruhit"];
+
+//tuple --> array --> order --> type of values
+let user : [string, number] = ["Iftekar", 22];
